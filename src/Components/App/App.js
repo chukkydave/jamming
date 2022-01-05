69903805d881f697193f2956ca17adbf5535aa09
chukkydave <chukkydave@gmail.com>
2022-01-05 11:21:49 +0100

    imported necesssary components for the app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import './App.css';
+import SearchResults from '../SearchResults/SearchResults';
+import SearchBar from '../SearchBar/SearchBar';
+import PlayList from '../PlayList/PlayList';
 
-function App() {
-	return (
-		<div>
-			<h1>
-				Ja<span className="highlight">mmm</span>ing
-			</h1>
-			<div className="App">
-				{/* Add a SearchBar component */}
-				<div className="App-playlist">
-					{/* Add a SearchResults component  */}
-					{/* Add a Playlist component  */}
+class App extends React.Component() {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchResults: [
+				{
+					name: 'rock',
+					artist: 'Olamide Baddo',
+					album: 'cappy-diem',
+					id: 3,
+				},
+				{
+					name: 'pepper them',
+					artist: 'Olamide Baddo',
+					album: 'cappy-diem',
+					id: 4,
+				},
+				{
+					name: 'bolanle',
+					artist: 'Olamide Baddo',
+					album: 'cappy-diem',
+					id: 3,
+				},
+			],
+		};
+	}
+	render() {
+		return (
+			<div>
+				<h1>
+					Ja<span className="highlight">mmm</span>ing
+				</h1>
+				<div className="App">
+					<SearchBar />
+					<div className="App-playlist">
+						<SearchResults searchResults={this.state.searchResults} />
+						<PlayList />
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
